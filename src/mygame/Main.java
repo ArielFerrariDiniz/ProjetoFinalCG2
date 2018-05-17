078345ea5fa67dc8fec30d42d044c1da3a1a8597
Marun <Marun@172.18.2.212>
2018-05-16 22:17:29 -0300

    Atualização com Dardo Com Ariel

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -2,10 +2,14 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.font.BitmapText;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -23,18 +27,37 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
-        //teste
+        
+        
         center();
+        CreateDart();
+        createLigth();
+        
     }
     
+    private void createLigth() {
+
+        DirectionalLight l1 = new DirectionalLight();
+        l1.setDirection(new Vector3f(1, -0.7f, 0));
+        rootNode.addLight(l1);
+
+        DirectionalLight l2 = new DirectionalLight();
+        l2.setDirection(new Vector3f(-1, 0, 0));
+        rootNode.addLight(l2);
+
+        DirectionalLight l3 = new DirectionalLight();
+        l3.setDirection(new Vector3f(0, 0, -1.0f));
+        rootNode.addLight(l3);
+
+        DirectionalLight l4 = new DirectionalLight();
+        l4.setDirection(new Vector3f(0, 0, 1.0f));
+        rootNode.addLight(l4);
+
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        rootNode.addLight(ambient);
+
+    }
     public void center(){
         
         guiNode.detachAllChildren();
@@ -50,11 +73,18 @@ public class Main extends SimpleApplication {
         
         guiNode.attachChild(center);
     }
-
-    @Override
+    public void CreateDart()
+    {
+        Spatial dart = assetManager.loadModel("folder/dart.obj");
+        dart.scale(0.02f);
+        dart.rotate(0,0,0);
+        dart.setLocalTranslation(-0.5f,-0.6f,+8);
+        rootNode.attachChild(dart);
+    }
     public void simpleUpdate(float tpf) {
         //TODO: add update code
     }
+    
 
     @Override
     public void simpleRender(RenderManager rm) {
